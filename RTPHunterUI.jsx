65d36bc8e1dbd7ba2850b60c5ccb9284ffddf529
gtgspot <github.tgspot@gmail.com>
2026-03-27 20:30:37 +1100

    Create RTPHunterUI.jsx for frontend UI

---
+++ RTPHunterUI.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const RTPHunterUI = () => {
+    return (
+        <div>
+            <h1>RTP Hunter UI</h1>
+            <p>Welcome to the RTP Hunter application!</p>
+            {/* Add your UI components here */}
+        </div>
+    );
+};
+
+export default RTPHunterUI;
